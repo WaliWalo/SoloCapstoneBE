e0de385de1ff1838c953c0e55d6835d6d7b4d1d9
Hung Jin Chong <hungjinchong@Outlook.com>
2021-03-31 10:30:19 +0100

    Debug
    Check if wyr score is emitting on select

--- src/socket.js
+++ src/socket.js
@@ -111,6 +111,7 @@ const createSocketServer = (server) => {
 
     socket.on("onSelect", ({ selection, roomName, userId }) => {
       //broadcast selection to room
+      console.log(selection);
       io.in(roomName).emit("onSelect", { selection, userId });
     });
 
